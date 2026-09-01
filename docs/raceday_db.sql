77fbfd56fb6beb82b321f3a59bff66e4134739a5
Pandelani Munenyiwa <ST10473181@rcconnect.edu.za>
2026-09-01 18:27:05 +0200

    Created the Events

--- docs/raceday_db.sql
+++ docs/raceday_db.sql
@@ -19,3 +19,22 @@ created_at datetime not null default getdate(),
 updated_at datetime null,
 is_active bit not null default 1
 );
+
+--creating the events table
+create table events(
+event_id int primary key identity(1,1),
+organiser_id int not null,
+event_name varchar(100) not null,
+event_description text not null,
+event_date datetime not null,
+location varchar(200) not null,
+distance decimal(5,2) not null,
+event_type varchar(20) not null check (event_type in ('Run', 'Walk', 'Cycle')),
+banner_image_url varchar(500) null,
+max_participants int null,
+entry_fee decimal(10,2) null,
+event_status varchar(20) not null default 'Upcoming' check (event_status in ('Upcoming', 'Ongoing', 'Completed', 'Cancelled')),
+created_at datetime not null default getdate(),
+updated_at datetime null,
+foreign key (organiser_id) references users(user_id)
+);
